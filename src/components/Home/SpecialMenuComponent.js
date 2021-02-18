5552ce711339750112b4e1d1a18e90b82827ac3b
Jeet009 <jeetmukherjee009@gmail.com>
2021-02-18 20:29:57 +0530

    working

--- src/components/Home/SpecialMenuComponent.js
+++ src/components/Home/SpecialMenuComponent.js
@@ -9,7 +9,6 @@ import mainCourseImgTwo from "../../assets/mainCourse/main-course-two.jpeg";
 import mainCourseImgThree from "../../assets/mainCourse/main-course-three.jpeg";
 import mainCourseImgFour from "../../assets/mainCourse/main-course-four.jpeg";
 import desertOne from "../../assets/desert/desertOne.jpeg";
-import desertTwo from "../../assets/desert/desertTwo.jpeg";
 import cookingSvg from "../../assets/cookingSvg.svg";
 
 function SpecialMenuComponent() {
@@ -217,6 +216,326 @@ function SpecialMenuComponent() {
     },
   ]);
 
+  //Non-Veg Starter
+  const [nonVegStarter] = useState([
+    {
+      id: 33,
+      name: "Chicken Tikka",
+      type: "Non Veg Chicken Starter",
+      price: "100 /-",
+    },
+    {
+      id: 34,
+      name: "Lebnani Tikka",
+      type: "Non Veg Chicken Starter",
+      price: "110 /-",
+    },
+    {
+      id: 35,
+      name: "Chicken Manchurian",
+      type: "Non Veg Chicken Starter",
+      price: "100 /-",
+    },
+    {
+      id: 36,
+      name: "Fish Tikka",
+      type: "Non Veg Chicken Starter",
+      price: "100 /-",
+    },
+    {
+      id: 37,
+      name: "Fish Finger",
+      type: "Non Veg Chicken Starter",
+      price: "100 /-",
+    },
+    {
+      id: 38,
+      name: "Chicken Tandoori",
+      type: "Non Veg Chicken Starter",
+      price: "299/- 159/-",
+    },
+    {
+      id: 39,
+      name: "Chilli Chicken",
+      type: "Non Veg Chicken Starter",
+      price: "90 /-",
+    },
+    {
+      id: 40,
+      name: "Egg Roll",
+      type: "Non Veg Chicken Starter",
+      price: "30 /-",
+    },
+    {
+      id: 41,
+      name: "Chicken Roll",
+      type: "Non Veg Chicken Starter",
+      price: "40 /-",
+    },
+    {
+      id: 42,
+      name: "Egg Chicken Roll",
+      type: "Non Veg Chicken Starter",
+      price: "50 /-",
+    },
+    {
+      id: 43,
+      name: "Egg Chow",
+      type: "Non Veg Chicken Starter",
+      price: "40 /-",
+    },
+    {
+      id: 44,
+      name: "Chicken Chow",
+      type: "Non Veg Chicken Starter",
+      price: "50 /-",
+    },
+    {
+      id: 45,
+      name: "Mixed Chow",
+      type: "Non Veg Chicken Starter",
+      price: "100 /-",
+    },
+    {
+      id: 46,
+      name: "Chicken Pizza",
+      type: "Non Veg Chicken Starter",
+      price: "100 /- 80/-",
+    },
+    {
+      id: 47,
+      name: "Dal Fry",
+      type: "Non Veg Chicken Starter",
+      price: "30 /-",
+    },
+    {
+      id: 48,
+      name: "kadhai Paneer",
+      type: "Non Veg Chicken Starter",
+      price: "90 /-",
+    },
+    {
+      id: 49,
+      name: "Palak Paneer",
+      type: "Non Veg Chicken Starter",
+      price: "95 /-",
+    },
+    {
+      id: 50,
+      name: "Palak Butter Masala",
+      type: "Non Veg Chicken Starter",
+      price: "100 /-",
+    },
+    {
+      id: 51,
+      name: "Mixed Veg Garden Surprise",
+      type: "Non Veg Chicken Starter",
+      price: "75 /-",
+    },
+  ]);
+
+  //Veg Main Course
+  const [vegMainCourse] = useState([
+    {
+      id: 47,
+      name: "Dal Fry",
+      type: "Main Course(Veg)",
+      price: "30 /-",
+    },
+    {
+      id: 48,
+      name: "Kadhai Paneer",
+      type: "Main Course(Veg)",
+      price: "90 /-",
+    },
+    {
+      id: 49,
+      name: "Palak Paneer",
+      type: "Main Course(Veg)",
+      price: "95 /-",
+    },
+    {
+      id: 50,
+      name: "Palak Butter Masala",
+      type: "Main Course(Veg)",
+      price: "100 /-",
+    },
+    {
+      id: 51,
+      name: "Mixed Veg Garden Surprise",
+      type: "Main Course(Veg)",
+      price: "75 /-",
+    },
+  ]);
+
+  //Non-Veg Main Course
+  const [nonVegMainCourse] = useState([
+    {
+      id: 52,
+      name: "Murg Matka",
+      type: "Main Course(Non Veg)",
+      price: "100 /-",
+    },
+    {
+      id: 53,
+      name: "Chicken Tikka Butter Masala",
+      type: "Main Course(Non Veg)",
+      price: "120 /-",
+    },
+    {
+      id: 54,
+      name: "Chicken Manchurian",
+      type: "Main Course(Non Veg)",
+      price: "95 /-",
+    },
+    {
+      id: 55,
+      name: "Chicken Biriyani",
+      type: "Main Course(Non Veg)",
+      price: "100 /-",
+    },
+    {
+      id: 56,
+      name: "Fish Curry",
+      type: "Main Course(Non Veg)",
+      price: "80 /-",
+    },
+    {
+      id: 57,
+      name: "Chicken Tikka Masala",
+      type: "Main Course(Non Veg)",
+      price: "100 /-",
+    },
+    {
+      id: 58,
+      name: "Plane Nun",
+      type: "Main Course(Non Veg)",
+      price: "15 /-",
+    },
+    {
+      id: 59,
+      name: "Romali Roti",
+      type: "Main Course(Non Veg)",
+      price: "15 /-",
+    },
+    {
+      id: 60,
+      name: "Butter Nun",
+      type: "Main Course(Non Veg)",
+      price: "20 /-",
+    },
+    {
+      id: 61,
+      name: "Lakcha Paratha",
+      type: "Main Course(Non Veg)",
+      price: "25 /-",
+    },
+  ]);
+
+  //Rice
+  const [rice] = useState([
+    {
+      id: 62,
+      name: "Jeera Rice",
+      type: "Rice",
+      price: "30 /-",
+    },
+    {
+      id: 63,
+      name: "Steam Rice",
+      type: "Rice",
+      price: "20 /-",
+    },
+    {
+      id: 64,
+      name: "Biriyani Rice",
+      type: "Rice",
+      price: "25 /-",
+    },
+    {
+      id: 65,
+      name: "Veg Polao",
+      type: "Rice",
+      price: "35 /-",
+    },
+    {
+      id: 66,
+      name: "Makai Polao",
+      type: "Rice",
+      price: "35 /-",
+    },
+    {
+      id: 67,
+      name: "Peas Polao",
+      type: "Rice",
+      price: "35 /-",
+    },
+    {
+      id: 68,
+      name: "Veg Polao",
+      type: "Rice",
+      price: "75 /-",
+    },
+  ]);
+
+  // Dessert
+  const [dessert] = useState([
+    {
+      id: 69,
+      name: "Vanilla",
+      type: "Dessert",
+      price: "30 /-",
+    },
+    {
+      id: 70,
+      name: "Strawberry",
+      type: "Dessert",
+      price: "30 /-",
+    },
+    {
+      id: 71,
+      name: "Chocolate",
+      type: "Dessert",
+      price: "35 /-",
+    },
+    {
+      id: 72,
+      name: "Mango",
+      type: "Dessert",
+      price: "30 /-",
+    },
+    {
+      id: 73,
+      name: "Golab Jamun",
+      type: "Dessert",
+      price: "25 /-",
+    },
+    {
+      id: 74,
+      name: "Finni",
+      type: "Dessert",
+      price: "35 /-",
+    },
+    {
+      id: 75,
+      name: "Green Salad",
+      type: "Dessert",
+      price: "30 /-",
+    },
+    {
+      id: 76,
+      name: "Fry Papad",
+      type: "Dessert",
+      price: "10 /-",
+    },
+    {
+      id: 77,
+      name: "Dahi Puri(9 pC)",
+      type: "Dessert",
+      price: "40 /-",
+    },
+  ]);
+
   // Menu List
   const [menu] = useState([
     {
@@ -247,7 +566,7 @@ function SpecialMenuComponent() {
     {
       id: 6,
       name: "Non-Veg-Main Course",
-      db_name: "nonVegManiCourse",
+      db_name: "nonVegMainCourse",
     },
     {
       id: 7,
@@ -278,10 +597,11 @@ function SpecialMenuComponent() {
             >
               {/* Drinks & Mocktail  */}
 
-              {data.db_name == "drinksMocktail" &&
+              {data.db_name === "drinksMocktail" &&
                 drinksMocktail.map((data) => (
                   <div
                     className="img-card shadow"
+                    key={data.id}
                     style={{
                       backgroundImage: "url(" + starterImgOne + ")",
                     }}
@@ -297,16 +617,19 @@ function SpecialMenuComponent() {
                         src={cookingSvg}
                         className="img-fluid"
                         width="50px"
+                        alt="..."
                       />
                     </div>
                   </div>
                 ))}
 
               {/* Milke Shake */}
-              {data.db_name == "milkShake" &&
+
+              {data.db_name === "milkShake" &&
                 milkShake.map((data) => (
                   <div
                     className="img-card shadow"
+                    key={data.id}
                     style={{
                       backgroundImage: "url(" + starterImgTwo + ")",
                     }}
@@ -322,6 +645,7 @@ function SpecialMenuComponent() {
                         src={cookingSvg}
                         className="img-fluid"
                         width="50px"
+                        alt="..."
                       />
                     </div>
                   </div>
@@ -329,10 +653,122 @@ function SpecialMenuComponent() {
 
               {/* Veg Starter  */}
 
-              {data.db_name == "vegStarter" &&
+              {data.db_name === "vegStarter" &&
                 vegStarter.map((data) => (
                   <div
                     className="img-card shadow"
+                    key={data.id}
+                    style={{
+                      backgroundImage: "url(" + mainCourseImgTwo + ")",
+                    }}
+                  >
+                    <div className="overlay-menu-card">
+                      <h4>{data.name}</h4>
+                      <h6>
+                        <b>Sky Lounge</b>
+                      </h6>
+                      <hr className="hr-custom" />
+                      <br />
+                      <img
+                        src={cookingSvg}
+                        className="img-fluid"
+                        width="50px"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                ))}
+
+              {/* Non Veg Starter  */}
+
+              {data.db_name === "nonVegStarter" &&
+                nonVegStarter.map((data) => (
+                  <div
+                    className="img-card shadow"
+                    key={data.id}
+                    style={{
+                      backgroundImage: "url(" + mainCourseImgOne + ")",
+                    }}
+                  >
+                    <div className="overlay-menu-card">
+                      <h4>{data.name}</h4>
+                      <h6>
+                        <b>Sky Lounge</b>
+                      </h6>
+                      <hr className="hr-custom" />
+                      <br />
+                      <img
+                        src={cookingSvg}
+                        className="img-fluid"
+                        width="50px"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                ))}
+
+              {/* Veg Main Course  */}
+
+              {data.db_name === "vegMainCourse" &&
+                vegMainCourse.map((data) => (
+                  <div
+                    className="img-card shadow"
+                    key={data.id}
+                    style={{
+                      backgroundImage: "url(" + mainCourseImgFour + ")",
+                    }}
+                  >
+                    <div className="overlay-menu-card">
+                      <h4>{data.name}</h4>
+                      <h6>
+                        <b>Sky Lounge</b>
+                      </h6>
+                      <hr className="hr-custom" />
+                      <br />
+                      <img
+                        src={cookingSvg}
+                        className="img-fluid"
+                        width="50px"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                ))}
+
+              {/* Non Veg Main Course  */}
+
+              {data.db_name === "nonVegMainCourse" &&
+                nonVegMainCourse.map((data) => (
+                  <div
+                    className="img-card shadow"
+                    key={data.id}
+                    style={{
+                      backgroundImage: "url(" + mainCourseImgOne + ")",
+                    }}
+                  >
+                    <div className="overlay-menu-card">
+                      <h4>{data.name}</h4>
+                      <h6>
+                        <b>Sky Lounge</b>
+                      </h6>
+                      <hr className="hr-custom" />
+                      <br />
+                      <img
+                        src={cookingSvg}
+                        className="img-fluid"
+                        width="50px"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                ))}
+
+              {/* Rice  */}
+              {data.db_name === "rice" &&
+                rice.map((data) => (
+                  <div
+                    className="img-card shadow"
+                    key={data.id}
                     style={{
                       backgroundImage: "url(" + mainCourseImgThree + ")",
                     }}
@@ -348,6 +784,34 @@ function SpecialMenuComponent() {
                         src={cookingSvg}
                         className="img-fluid"
                         width="50px"
+                        alt="..."
+                      />
+                    </div>
+                  </div>
+                ))}
+
+              {/* Dessert  */}
+              {data.db_name === "dessert" &&
+                dessert.map((data) => (
+                  <div
+                    className="img-card shadow"
+                    key={data.id}
+                    style={{
+                      backgroundImage: "url(" + desertOne + ")",
+                    }}
+                  >
+                    <div className="overlay-menu-card">
+                      <h4>{data.name}</h4>
+                      <h6>
+                        <b>Sky Lounge</b>
+                      </h6>
+                      <hr className="hr-custom" />
+                      <br />
+                      <img
+                        src={cookingSvg}
+                        className="img-fluid"
+                        width="50px"
+                        alt="..."
                       />
                     </div>
                   </div>
